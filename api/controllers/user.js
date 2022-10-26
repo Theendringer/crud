046dc28d73f0883015a0a20470978154c2ad01ec
theendringer <gustepereira@gmail.com>
2022-10-26 16:27:12 -0300

    commit

---
+++ api/controllers/user.js
@@ -0,0 +1,14 @@
+import { db } from "../db.js"
+
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM salas"
+
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+
+        return res.status(200).json(data)
+    })
+
+
+}
---
+++ api/db.js
@@ -0,0 +1,9 @@
+import mysql from "mysql"
+
+export const db = mysql.createConnection({
+    host: "35.239.140.215",
+    user: "root",
+    password: "yRCpYf&btGx/5.Gc",
+    database: "unisalas"
+    
+})
