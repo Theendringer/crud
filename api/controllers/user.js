5c3d340faf3f9fce67220aedf110e033bc53c202
theendringer <gustepereira@gmail.com>
2022-10-27 15:32:18 -0300

    commit

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,3 +1,4 @@
+import { response } from "express"
 import { db } from "../db.js"
 
 
@@ -11,4 +12,51 @@ export const getUsers = (_, res) => {
     })
 
 
+}
+
+
+export const addSala = (req, res) => {
+    const q =
+        "INSERT INTO salas(`nome`, `codigo`, `descricao`) VALUES(?)"
+
+    const values = [
+        req.body.nome,
+        req.body.codigo,
+        req.body.descricao,
+    ]
+
+    db.query(q, [values], (err) =>{
+        if (err) return res.json(err)
+
+        return res.status(200).json("Sala criada com sucesso!")
+    })
+}
+
+
+export const updateSala = (req, res) =>{
+    const q = 
+    "UPDATE sala SET `nome` = ?, `codigo` = ?, `descricao` = ? WHERE `id` = ?"
+
+    const values = [
+        req.body.nome,
+        req.body.codigo,
+        req.body,descricao,
+    ]
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err)
+
+        return res.status(200).json("Sala editada com sucesso!")
+    })
+}
+
+
+export const deleteSala = (req, res) => {
+    const q = "DELETE FROM salas WHERE `id` = ?"
+
+    db.query(q, [res.params.id], (err) =>{
+        if (err) return res.json(err)
+        
+        return res.status(200).json("Sala deleltada com sucesso!")
+    })
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { getUsers } from "..//controllers/user.js"
+import { getUsers, addSala, updateSala, deleteSala } from "..//controllers/user.js"
 
 const router = express.Router()
 
 router.get("/", getUsers)
 
+router.post("/", addSala)
+
+router.put("/:id", updateSala)
+
+router.delete("/:id", deleteSala)
 
 
 export default router
